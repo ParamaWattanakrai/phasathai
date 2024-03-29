bfb52610772374fa81c8ffc9b0e96b8ef3dc7703
ParamaWattanakrai <Parama.Wattanakrai@gmail.com>
2024-03-29 20:30:01 +0700

    consonant sound

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const HIGHLIGHTDICT = {
+const HIGHLIGHT_DICT = {
     '.phonology-trigger': '.phonology', '.manner-of-articulation-trigger': '.manner-of-articulation', '.unaspirated-plosive-trigger': '.unaspirated-plosive', '.aspirated-plosive-fricative-trigger': '.aspirated-plosive-fricative', '.nasal-trigger': '.nasal',
     '.articulator-trigger': '.articulator', '.velar-trigger': '.velar', '.palatal-trigger': '.palatal', '.alveolar-trigger': '.alveolar', '.labial-trigger': '.labial',
     '.column-trigger': '.column', '.first-column-trigger': '.first-column', '.second-column-trigger': '.second-column', '.third-column-trigger': '.third-column', '.forth-column-trigger': '.forth-column', '.fifth-column-trigger': '.fifth-column',
@@ -6,52 +6,43 @@ const HIGHLIGHTDICT = {
     '.consonant-class-trigger': '.consonant-class', '.mid-consonant-trigger': '.mid-consonant', '.high-consonant-trigger': '.high-consonant', '.low-consonant-trigger': '.low-consonant', '.paired-low-consonant-trigger': '.paired-low-consonant', '.unpaired-low-consonant-trigger': '.unpaired-low-consonant',
     '.trill-approximant-trigger': '.trill-approximant', '.fricative-trigger': '.fricative', '.alveolar-lateral-approximant-trigger': '.alveolar-lateral-approximant', '.glottal-stop-trigger': '.glottal-stop',  '.glottal-fricative-trigger': '.glottal-fricative',
     '.extra-row-trigger': '.extra-row',
+    '.k':'.k','.kh':'.kh','.ng':'.ng','.c':'.c','.ch':'.ch','.y':'.y','.d':'.d','.t':'.t','.th':'.th','.n':'.n','.b':'.b','.p':'.p','.ph':'.ph','.f':'.f','.m':'.m','.r':'.r','.l':'.l','.w':'.w','.s':'.s','.h':'.h','.o':'.o'
 }
 
-function rgbToHsl(rgbString) {
-    var match = rgbString.match(/\d+/g);
-    var r = parseInt(match[0]);
-    var g = parseInt(match[1]);
-    var b = parseInt(match[2]);
-    
-    console.log(r, g, b)
+const CONSONANT_SOUNDS_LIST = ['k','kh','ng','c','ch','y','d','t','th','n','b','p','ph','f','m','r','l','w','s','h','o']
 
-    r /= 255, g /= 255, b /= 255;
+const CONSONANTS = document.querySelectorAll('.consonant')
 
-    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-    var h, s, l = (max + min) / 2;
-
-    if (max == min) {
-        h = s = 0; // achromatic
-    } else {
-        var d = max - min;
-        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-
-        switch (max) {
-        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-        case g: h = (b - r) / d + 2; break;
-        case b: h = (r - g) / d + 4; break;
+function haveCommonElements(arr1, arr2){
+    for(let i=0; i<arr1.length; i++){
+        for(let j=0; j<arr2.length; j++){
+            if(arr1[i] == arr2[j]){
+                return true;
+            }
         }
-
-        h /= 6;
     }
-
-    h = h * 360
-    s = s * 100
-    l = l * 100
-    
-    return [ h, s, l ];
+    return false;
 }
 
-const toggleHighlight = (key) => {
+const toggleHighlight = (triggeredElement, key) => {
     console.log(key)
-    console.log(HIGHLIGHTDICT[key])
-    let selfElements = document.querySelectorAll(key);
-    let affectedElements = document.querySelectorAll(HIGHLIGHTDICT[key]);
+    console.log(HIGHLIGHT_DICT[key])
+    let allKeyElements = document.querySelectorAll(key);
+    let valueElements = document.querySelectorAll(HIGHLIGHT_DICT[key]);
+
+    let unhighlightedElements = document.querySelectorAll('.unhighlight')
     let highlightedElements = document.querySelectorAll('.group-highlight')
     let highlightedTriggerElements = document.querySelectorAll('.group-trigger-highlight')
 
-    if (highlightedElements.length > 0) {
+    if (key == HIGHLIGHT_DICT[key]) {
+        allKeyElements = [triggeredElement]
+    }
+
+    if ((highlightedElements.length + unhighlightedElements.length) > 0) {
+        console.log('huh')
+        CONSONANTS.forEach((element) => {
+            element.classList.remove('unhighlight')
+        })
         highlightedElements.forEach((element) => {
             element.classList.remove('group-highlight')
         })
@@ -60,21 +51,33 @@ const toggleHighlight = (key) => {
         })
     }
 
-    if (selfElements[0] !== highlightedTriggerElements[0]) {
-        selfElements.forEach((element) => {
+    if (allKeyElements[0] !== highlightedTriggerElements[0]) {
+        consonantKeyHighlight = haveCommonElements(allKeyElements, CONSONANTS)
+        consonantValueHighlight = haveCommonElements(valueElements, CONSONANTS)
+        if (consonantKeyHighlight || consonantValueHighlight) {
+            console.log('how')
+            CONSONANTS.forEach((element) => {
+                element.classList.add('unhighlight')
+            })
+        }
+        allKeyElements.forEach((element) => {
+            element.classList.remove('unhighlight')
             element.classList.add('group-trigger-highlight')
         });
-        affectedElements.forEach((element) => {
-            element.classList.add('group-highlight')
+        valueElements.forEach((element) => {
+            element.classList.remove('unhighlight')
+            if (!element.classList.contains('consonant')) {
+                element.classList.add('group-highlight')
+            }
         });
     }
 }
 
-Object.keys(HIGHLIGHTDICT).forEach((key) => {
+Object.keys(HIGHLIGHT_DICT).forEach((key) => {
     let elements = document.querySelectorAll(key);
     elements.forEach((element) => {
         element.addEventListener('click', () => {
-            toggleHighlight(key)
+            toggleHighlight(element, key)
         })
     })
 });
